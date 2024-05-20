5d4e02a9f702eab6ff02f2d839c00f6b85bb7356
glucrypto <glucrypto@protonmail.com>
2024-05-20 19:36:36 -0400

    fix: updated interaction defaults

--- src/read/index.ts
+++ src/read/index.ts
@@ -1,3 +1,10 @@
-export { interactionsByTypeKey } from "./interactionsByTypeKey.js";
+export {
+  interactionsByKey,
+  interactionsByKeyPage,
+} from "./interactionByKey.js";
+export {
+  interactionsByTypeKey,
+  interactionsByTypeKeyPage,
+} from "./interactionsByTypeKey.js";
 export { user } from "./user.js";
 export { users } from "./users.js";
