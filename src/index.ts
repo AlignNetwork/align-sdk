f0f1aa5facf03de47aee233f0b780c22f7106752
glucrypto <glucrypto@protonmail.com>
2024-05-21 14:44:42 -0400

    fix: added exports for interactions

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export { AlignDefaults } from "./dataNetwork/defaults.js";
 export { getContent, getPins } from "./dataNetwork/read.js";
 export { validate } from "./dataNetwork/validate.js";
 export * from "./hooks";
+export { createInteractionType } from "./interactions/createInteractionType.js";
+export { interactOnchain } from "./interactions/interact.js";
 export * from "./lib/abi/alignIdRegistryABI";
 export * from "./lib/abi/interactionStationABI";
 export * from "./lib/constants";
