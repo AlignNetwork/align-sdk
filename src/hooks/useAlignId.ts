a409de57aeaad850d852cc49621c389a705d2aeb
glucrypto <glucrypto@protonmail.com>
2024-05-22 11:30:29 -0400

    fix: added undefined to alignid hook

--- src/hooks/useAlignId.ts
+++ src/hooks/useAlignId.ts
@@ -1,14 +1,13 @@
 "use client";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getId } from "../read";
 
-export function useAlignId(id: `0x${string}`) {
-  return useInfiniteQuery({
+export function useAlignId(id: `0x${string}` | undefined) {
+  return useQuery({
     queryKey: ["alignid"],
-    initialPageParam: id,
     queryFn: async () => {
+      if (!id) return undefined;
       return await getId(id);
     },
-    getNextPageParam: (lastPage: any) => lastPage.nextPage,
   });
 }
