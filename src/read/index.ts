ddfe7cf4108f277e310d290114f24a7dd6162a13
glucrypto <glucrypto@protonmail.com>
2024-05-23 13:17:54 -0400

    fix: added get by parent type

--- src/read/index.ts
+++ src/read/index.ts
@@ -4,5 +4,6 @@ export { getId } from "./id.js";
 export { interactionsByKeyPage } from "./interactionByKey.js";
 export { interactionTypeKeyDefinition } from "./interactionTypeDefinition.js";
 export { interactionsByTypeKeyPage } from "./interactionsByTypeKey.js";
+export { itypeByParentKey } from "./itypebyParentKey.js";
 export { user } from "./user.js";
 export { users } from "./users.js";
