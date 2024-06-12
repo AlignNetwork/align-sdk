0f3f747266da0786b6dafb946f1ac9a57412e64c
glucrypto <glucrypto@protonmail.com>
2024-06-12 15:42:57 -0400

    fix: updated get content to work with different urls

--- src/dataNetwork/read.ts
+++ src/dataNetwork/read.ts
@@ -1,17 +1,22 @@
-import { alignUrls } from "../lib";
+import { alignEnvironment } from "../lib";
 
-const dataNetworkUrl = alignUrls.ipfs;
-
-export async function getPins(): Promise<any> {
-  const res = await fetch(`${dataNetworkUrl}/getpins`, {
+export async function getPins(
+  env: "development" | "production" = "development"
+): Promise<any> {
+  let url = `${alignEnvironment(env).ipfs}/getpins`;
+  const res = await fetch(`${url}`, {
     method: "POST",
   });
   const result = await res.json();
   return result;
 }
 
-export async function getContent(cid: string): Promise<any> {
-  const res = await fetch(`${dataNetworkUrl}/ipfs/${cid}`, {
+export async function getContent(
+  cid: string,
+  env: "development" | "production" = "development"
+): Promise<any> {
+  let url = `${alignEnvironment(env).ipfs}/ipfs/${cid}`;
+  const res = await fetch(`${url}`, {
     method: "GET",
   });
   const result = await res.json();
