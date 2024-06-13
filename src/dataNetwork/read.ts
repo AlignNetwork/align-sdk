8f975b2fc455095baa9838165857ec7256ab9507
glucrypto <glucrypto@protonmail.com>
2024-06-13 11:50:00 -0400

    fix: added array buffer getter

--- src/dataNetwork/read.ts
+++ src/dataNetwork/read.ts
@@ -22,3 +22,28 @@ export async function getContent(
   const result = await res.json();
   return result;
 }
+
+export async function getArrayBuffer(
+  cid: string,
+  env: "development" | "production" = "development"
+): Promise<any> {
+  let url = `${alignEnvironment(env).ipfs}/arraybuffer/${cid}`;
+  const res = await fetch(`${url}`, {
+    method: "GET",
+  });
+  const result = await res.arrayBuffer();
+  console.log("cid:", result);
+  const arrayBuffer = new Uint8Array(result).buffer;
+  const base64String = arrayBufferToBase64(arrayBuffer);
+  return base64String;
+}
+
+function arrayBufferToBase64(buffer: ArrayBuffer) {
+  let binary = "";
+  const bytes = new Uint8Array(buffer);
+  const len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binary);
+}
