1fde70f2efdeee505f1402e3c68e466399eae2ca
glucrypto <glucrypto@protonmail.com>
2024-06-21 13:20:57 -0400

    fix: exported getalignEvnrionment

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,17 @@ export {
   getStorage,
 } from "./dataNetwork/read.js";
 export { verifySignature } from "./dataNetwork/signature.js";
+export * from "./dataNetwork/upload.js";
 export { validate } from "./dataNetwork/validate.js";
 export * from "./hooks";
 export { createInteractionType } from "./interactions/createInteractionType.js";
 export { interactOnchain } from "./interactions/interact.js";
 export * from "./lib/abi/alignIdRegistryABI";
 export * from "./lib/abi/interactionStationABI";
+export { getAlignEnvironment } from "./lib/alignEnvironment";
 export * from "./lib/constants";
 export { alignEnvironment, alignTestnetV2 } from "./lib/constants.js";
 export { interactDefaults } from "./main.js";
 export * from "./read";
 export { myInteractionByTo } from "./read/myInteractionByTo.js";
 export { myInteractionsByType } from "./read/myinteractionsbytype.js";
-
-export * from "./dataNetwork/upload.js";
